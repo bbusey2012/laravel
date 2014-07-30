44526278e62e64ae6a0ba3d3040d27f8102ef166
Brendan Busey <brendan.busey@assistance.net>
2014-07-29 21:57:12 -0400

    Logout code for Matt to look at

---
+++ app/controllers/UserController.php
@@ -0,0 +1,150 @@
+<?php
+class UserController extends BaseController
+{
+	
+	function showHomePage()
+	{
+		return View::make('home');  
+	}
+
+	
+	function showCreateAccount()
+	{
+		return View::make('create_account');
+	}
+	
+	
+	function showLoginPage()
+	{
+		return View::make('login_form');
+	}
+	
+	function showLogoutPage()
+	{
+		return View::make('logout');
+	}
+	
+	function loginSuccess()
+	{
+		return View::make('loginSuccessful');
+	}
+
+	function doLogout()
+	{
+		Auth::logout();
+		//echo "You have been logged out!";
+		return Redirect::to('/home')->with('message', 'You have been successuflly logged out!');
+	}
+
+	
+	function doRegistration()
+	{
+		//get all the data from the create account form
+		$userData = Input::all();
+
+		//The sanitizing rules that form must pass before the user's info can be put in the database
+		//alpha checks to me sure that the user has entered only alphabetical characters
+		//required makes the field a required field
+
+		$validationRules = array(
+			'firstname' => array('required'),
+			'lastname' => array('required'),
+			'e-mail' => array('email','required'),
+			'username' => array('required'),
+			'password' => array('required'),
+		);
+
+		//How you make your own error messages
+		$customErrors = array(
+			'required' => 'This is a required field',
+			'unique' => 'Username is taken',
+			'alpha' => 'May only contain letters',
+			'alpha_num' => 'May only contain letters and numbers',
+			'email' => array(
+				'required' => 'Must be a valid email address',
+				'unique' => 'Email address already in use',
+				),
+			'confirmed' => 'Must match the above password',
+			'numeric' => 'Must contain at least one number',
+			);
+
+		//makes the validator that will validate the form using the validation rules 
+		$validator = Validator::make($userData,$validationRules,$customErrors);
+
+
+		//If form passes sanitizating then add user to database
+		if($validator->passes())
+		{
+			DB::table('users')->insert(
+	        array("first_name" => Input::get('firstname'),
+	              "last_name" => Input::get("lastname"),
+	              "e-mail" => Input::get("e-mail"),
+	              "username" => Input::get("username"),
+	              "password" => Hash::make(Input::get("password")),	
+	        	)
+	    	);
+
+			/*
+			//Sends e-mail to user thanking them for registering
+			Mail::send('emails.welcome', array('firstname'=>Input::get('firstname')), function($message)
+			{
+        		$message->to(Input::get('e-mail'), Input::get('firstname').' '.Input::get('lastname'))->subject('Welcome to the Laravel 4 Auth App!');
+    		});
+    		*/
+
+            return Redirect::to('/login')->with('message','Thank you for registering!');
+		}
+
+		//Otherwise, return user to create account page, 
+		//showing them the errors they made when they filled out the create account form
+		if($validator->fails())
+		{
+			return Redirect::to('/create')->withErrors($validator);
+		}
+	}
+	
+
+	function doLogin()
+	{
+		$loginInfo = Input::all();
+
+		$validationRules = array('username' => 'required', 'password' => 'required',);
+
+		$customErrors = array('required' => 'This is a required field');
+
+		$validator = Validator::make($loginInfo,$validationRules,$customErrors);
+
+		if ($validator->fails()) 
+		{
+			return Redirect::to('/login')
+			->withErrors($validator) // send back all errors to the login form
+			->withInput(Input::old('password')); // send back the input (not the password) so that we can repopulate the form
+		}
+
+		else 
+		{
+
+			// create our user data for the authentication
+			$userdata = array(
+				'username' 	=> Input::get('username'),
+				'password' 	=> Input::get('password')
+			);
+
+
+			//attempt to do the login
+			if (Auth::attempt($userdata)) 
+			{
+				return Redirect::to('/dashboard');
+			} 
+
+			else 
+			{
+
+				// send back the input (not the password) so that we can repopulate the form
+				return Redirect::to('/login')->with('message','Incorrect username or password')->withInput(Input::except('password')); 
+			}
+		}
+	}
+}
+
+?>
---
+++ app/routes.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('hello'); 
+});
+
+Route::get('/about', function()
+{
+	return View::make('about');
+});
+
+Route::get('/home','UserController@showHomePage');
+
+Route::get('/login','UserController@showLoginPage');
+
+Route::get('/logout','UserController@doLogout');
+
+Route::get('/dashboard','UserController@loginSuccess');
+
+Route::get('/create','UserController@showCreateAccount');
+
+Route::post('/registration','UserController@doRegistration');
+
+Route::post('/authentication','UserController@doLogin');
+
+Route::get('/adding_column', function()
+{
+	return View::make('database.adding_column_database');
+});
+
+Route::get('/adding_table', function()
+{
+	return View::make('database.making_database_table');
+});
+?>
